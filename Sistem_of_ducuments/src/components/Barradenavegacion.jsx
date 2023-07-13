7df2651e9d40b091dc52420bb59967ac20f985ad
Alex Bedon <alexanderbedon58724222@gmail.com>
2023-07-12 19:59:03 -0500

    create logo, create barra de navegacion

---
+++ Sistem_of_ducuments/src/components/Barradenavegacion.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Barradenavegacion = () => {
+    return (
+        <div>
+            <div>
+                <img src="Sistema_de_gestion_de_tramites\Sistem_of_ducuments\src\assets\pictures\logo.png" />
+                <h1>Agile Procedures</h1>
+                <div>
+                <ul>
+                    <li>
+                        Iniciar
+                    </li>
+                    <li>
+                        Registrarse
+                    </li>
+
+                </ul>
+                </div>
+            </div>
+            
+            <div>
+                <span>Sobre nosotros</span>
+            </div>
+        </div>
+    );
+};
+
+export default Barradenavegacion;
